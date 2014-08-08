8798fc64b1d46e96dc49874d52ed380a876421a8
Glen Pink <glen.pink@gmail.com>
2014-08-08 15:56:11 +1000

    Forking from zpar at http://sourceforge.net/projects/zpar/

---
+++ doc/compile.pl
@@ -0,0 +1,42 @@
+#!perl
+
+use warnings;
+use strict;
+
+my $command = "";
+my $compiler = "htlatex";
+my $change_dir  = "chdir";
+
+$command = "$compiler index";
+print "$command\n";
+system($command);
+
+chdir("doc");
+
+$command = "$compiler segmentor";
+print "$command\n";
+system($command);
+
+$command = "$compiler conparser";
+print "$command\n";
+system($command);
+
+$command = "$compiler deppar";
+print "$command\n";
+system($command);
+
+$command = "$compiler joint_seg_tag";
+print "$command\n";
+system($command);
+
+$command = "$compiler eng_tagger";
+print "$command\n";
+system($command);
+
+$command = "$compiler independent";
+print "$command\n";
+system($command);
+
+$command = "$compiler ccg";
+print "$command\n";
+system($command);
