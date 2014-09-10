fad85aab7b9aae6e6a1e2c18f471e879cdb078dc
Tim Dawborn <tim.dawborn@gmail.com>
2014-09-10 14:18:15 +1000

    More virtual removal.

--- src/english/dependency/label/stanford.h
+++ src/english/dependency/label/stanford.h
@@ -142,9 +142,7 @@ const unsigned long STANFORD_DEP_COUNT_BITS = 6;
  *==============================================================*/
 
 class CDependencyLabel {
-
 public:
-
    enum {NONE=0};
    enum {ROOT=1};
    enum {FIRST=1};
@@ -153,49 +151,38 @@ public:
    enum {SIZE=STANFORD_DEP_COUNT_BITS};
 
 protected:
-
    unsigned long m_code;
 
 public:
-
    CDependencyLabel() : m_code(NONE) {}
    CDependencyLabel(const unsigned long &code) : m_code(code) { }
-   CDependencyLabel(const std::string &str) { load(str); }
-   virtual ~CDependencyLabel() {}
+   explicit CDependencyLabel(const std::string &str) { load(str); }
+   ~CDependencyLabel() {}
 
 public:
+   inline const unsigned long &code() const { return m_code; }
+   inline const unsigned long &hash() const { return m_code; }
+   inline const std::string &str() const { return STANFORD_DEP_STRINGS[m_code]; }
 
-   const unsigned long &hash() const { return m_code; }
-   bool operator == (const CDependencyLabel &l) const { return m_code == l.m_code; }
-   bool operator != (const CDependencyLabel &l) const { return m_code != l.m_code; }
-   bool operator < (const CDependencyLabel &l) const { return m_code < l.m_code; }
-   bool operator > (const CDependencyLabel &l) const { return m_code > l.m_code; }
-   bool operator <= (const CDependencyLabel &l) const { return m_code <= l.m_code; }
-   bool operator >= (const CDependencyLabel &l) const { return m_code >= l.m_code; }
+   inline bool operator == (const CDependencyLabel &l) const { return m_code == l.m_code; }
+   inline bool operator != (const CDependencyLabel &l) const { return m_code != l.m_code; }
+   inline bool operator < (const CDependencyLabel &l) const { return m_code < l.m_code; }
+   inline bool operator > (const CDependencyLabel &l) const { return m_code > l.m_code; }
+   inline bool operator <= (const CDependencyLabel &l) const { return m_code <= l.m_code; }
+   inline bool operator >= (const CDependencyLabel &l) const { return m_code >= l.m_code; }
 
+   inline void load(const unsigned long &u) { m_code = u; }
    void load(const std::string &str) { 
       m_code = STANFORD_DEP_NONE;
       for (int i=FIRST; i<COUNT; ++i) {
-         if (STANFORD_DEP_STRINGS[i]==str) {
+         if (STANFORD_DEP_STRINGS[i] == str) {
             m_code = i;
             return;
          }
       }
    }
-
-   void load(const unsigned long &u) { 
-      m_code = u;
-   }
-
-   const std::string &str() const { 
-      return STANFORD_DEP_STRINGS[ m_code ]; 
-   }
-
-   const unsigned long &code() const {
-      return m_code;
-   }
-
 };
+
 
 inline std::istream & operator >> (std::istream &is, CDependencyLabel &label) {
    std::string s;
