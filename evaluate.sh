7f1b2b2b72cca9f84a1086e3bff4ac7ed77291ac
Tim Dawborn <tim.dawborn@gmail.com>
2014-09-11 15:58:43 +1000

    Allow nthread and niterations to be varied on the evaluation scripts command line.

--- evaluate.sh
+++ evaluate.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 set -e
 
-DATA=$(dirname ${0})/data
+NITERATIONS=10
 if [[ ${#} -gt 0 ]]; then
-  DATA=${1}
+  NITERATIONS=${1}
+  shift
 fi
 
-NITERATIONS=10
-MODEL=${DATA}/train.model
-TRAIN_GOLD=${DATA}/wsj_02-21.mxpost.sd
-TEST_GOLD=${DATA}/wsj_22.ontonotes.mxpost.sd
-TEST_RUN=${DATA}/wsj_22.ontonotes.mxpost.slash
+NTHREADS=1
+if [[ ${#} -gt 0 ]]; then
+  NTHREADS=${1}
+  shift
+fi
 
-TEST_OUTPUT=${DATA}/out
+DATA_DIR=$(dirname ${0})/data
+if [[ ${#} -gt 0 ]]; then
+  DATA_DIR=${1}
+  shift
+fi
 
-rm -f ${MODEL}*
-./dist/english.depparser/train ${TRAIN_GOLD} ${MODEL} ${NITERATIONS}
-mv ${MODEL}.0 ${MODEL}
+MODEL_DIR=$(dirname ${0})/models
+if [[ ${#} -gt 0 ]]; then
+  MODEL_DIR=${1}
+  shift
+fi
+
+TRAIN_GOLD=${DATA_DIR}/wsj_02-21.ontonotes.mxpost.sd
+TEST_GOLD=${DATA_DIR}/wsj_22.ontonotes.mxpost.sd
+TEST_RUN=${DATA_DIR}/wsj_22.ontonotes.mxpost.slash
+
+MODEL_NAME=$(basename ${TRAIN_GOLD})_${NITERATIONS}_${NTHREADS}
+MODEL=${MODEL_DIR}/${MODEL_NAME}.model
+TEST_OUTPUT=${MODEL_DIR}/${MODEL_NAME}.out
+
+rm -f ${MODEL_DIR}/${MODEL_NAME}*
+
+./dist/english.depparser/train ${TRAIN_GOLD} ${MODEL} ${NITERATIONS} ${NTHREADS}
 ./dist/english.depparser/depparser ${TEST_RUN} ${TEST_OUTPUT} ${MODEL}
-python3 evaluate_nbest.py ${TEST_OUTPUT} ${TEST_GOLD}
+python3 evaluate_nbest.py ${TEST_OUTPUT} ${TEST_GOLD} > ${MODEL_DIR}/${MODEL_NAME}.results
+
+cat ${MODEL_DIR}/${MODEL_NAME}.results
