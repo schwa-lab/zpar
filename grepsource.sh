3d4147ccc84f37055d11f68cba438b852e08063a
Tim Dawborn <tim.dawborn@gmail.com>
2014-09-14 10:22:27 +1000

    Added forgotten quoting of args.

--- grepsource.sh
+++ grepsource.sh
@@ -5,4 +5,4 @@ find src -type f \( -name '*.h' -or -name '*.cpp' \) \
   | egrep -v '^src/(chinese|generic)/' \
   | egrep -v '^src/common/(conparser|deplabeler|tagger)/' \
   | egrep -v '^src/common/depparser/implementations/(acl11|cad|covington|eisner|emnlp08|meta|punct|uppsala)/' \
-  | xargs egrep --color ${@}
+  | xargs egrep --color "${@}"
