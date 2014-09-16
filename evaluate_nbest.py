4d62db51722763f464f20ec3ff5584be791bac09
Dominick Ng <dominick8n@gmail.com>
2014-09-16 14:54:58 +1000

    * adding label error breakdown in eval

--- evaluate_nbest.py
+++ evaluate_nbest.py
@@ -1,6 +1,9 @@
 import argparse
 import re
 
+from collections import defaultdict
+from operator import itemgetter
+
 # malt tab indices
 WORD_INDEX = 0
 POS_INDEX = 1
@@ -37,6 +40,9 @@ def main(args):
     total_larcs = 0
     incorrect_larcs = 0
     skipped_larcs = 0
+    correct_label = defaultdict(int)
+    incorrect_label = defaultdict(int)
+    skipped_label = defaultdict(int)
 
     parse_index = 0
     total_sentences = 0
@@ -58,6 +64,9 @@ def main(args):
     current_total_larcs = 0
     current_incorrect_larcs = 0
     current_skipped_larcs = 0
+    current_correct_label = defaultdict(int)
+    current_incorrect_label = defaultdict(int)
+    current_skipped_label = defaultdict(int)
     local_parses = 0
     current_deps = set()
 
@@ -71,6 +80,9 @@ def main(args):
         local_incorrect_larcs = 0
         local_skipped_larcs = 0
         local_parses += 1
+        local_correct_label = defaultdict(int)
+        local_incorrect_label = defaultdict(int)
+        local_skipped_label = defaultdict(int)
         dep_list = []
         test_words = [w[WORD_INDEX] for w in test_sent]
 
@@ -93,6 +105,12 @@ def main(args):
                 correct_larcs += current_correct_larcs
                 skipped_larcs += current_skipped_larcs
                 incorrect_larcs += current_incorrect_larcs
+                for key, val in current_correct_label.items():
+                  correct_label[key] += val
+                for key, val in current_incorrect_label.items():
+                  incorrect_label[key] += val
+                for key, val in current_skipped_label.items():
+                  skipped_label[key] += val
                 oracle_lranks.append(current_best_lparse_index)
                 if current_incorrect_larcs == 0:
                     total_lcorrect_sentences += 1
@@ -107,6 +125,9 @@ def main(args):
             current_skipped_larcs = 0
             current_best_uparse_index = 0
             current_best_lparse_index = 0
+            current_correct_label = defaultdict(int)
+            current_incorrect_label = defaultdict(int)
+            current_skipped_label = defaultdict(int)
             total_parses += local_parses
             total_unique_parses += len(current_deps)
             local_parses = 0
@@ -126,17 +147,21 @@ def main(args):
                 # skip punct
                 local_skipped_uarcs += 1
                 local_skipped_larcs += 1
+                local_skipped_label[t[LABEL_INDEX]] += 1
             else:
                 dep_list.append(tuple(t))
                 if t[HEAD_INDEX] == g[HEAD_INDEX]:
                     local_correct_uarcs += 1
                     if t[LABEL_INDEX] == g[LABEL_INDEX]:
                         local_correct_larcs += 1
+                        local_correct_label[t[LABEL_INDEX]] += 1
                     else:
                         local_incorrect_larcs += 1
+                        local_incorrect_label[t[LABEL_INDEX]] += 1
                 else:
                     local_incorrect_uarcs += 1
                     local_incorrect_larcs += 1
+                    local_incorrect_label[t[LABEL_INDEX]] += 1
 
         if local_correct_uarcs > current_correct_uarcs:
             current_correct_uarcs = local_correct_uarcs
@@ -149,6 +174,9 @@ def main(args):
             current_correct_larcs = local_correct_larcs
             current_incorrect_larcs = local_incorrect_larcs
             current_skipped_larcs = local_skipped_larcs
+            current_correct_label = local_correct_label
+            current_incorrect_label = local_incorrect_label
+            current_skipped_label = local_skipped_label
             current_total_larcs = local_total_larcs
             current_best_lparse_index = parse_index
 
@@ -168,6 +196,12 @@ def main(args):
     correct_larcs += current_correct_larcs
     skipped_larcs += current_skipped_larcs
     incorrect_larcs += current_incorrect_larcs
+    for key, val in current_correct_label.items():
+      correct_label[key] += val
+    for key, val in current_incorrect_label.items():
+      incorrect_label[key] += val
+    for key, val in current_skipped_label.items():
+      skipped_label[key] += val
     oracle_lranks.append(current_best_lparse_index)
 
     total_parses += local_parses
@@ -185,6 +219,11 @@ def main(args):
     print("Parses per sentence: {:.2f} ({} / {})".format(total_parses / float(total_sentences), total_parses, total_sentences))
     print("Unique parses per sentence: {:.2f} ({} / {})".format(total_unique_parses / float(total_sentences), total_unique_parses, total_sentences))
 
+    if args.label_errors:
+      print("\nTotal label errors")
+      for label, val in sorted(incorrect_label.items(), reverse=True, key=itemgetter(1)):
+        print("{}: {}".format(label, val))
+
 if __name__ == "__main__":
     ap = argparse.ArgumentParser(description="Runs an evaluation over nbest dependency parse output")
     ap.add_argument("test_file", type=open, help="test nbest file to evaluate")
@@ -192,6 +231,7 @@ if __name__ == "__main__":
     ap.add_argument("--eval-punct", action="store_true", default=False, help="Evaluate punctuation")
     ap.add_argument("--oracle", action="store_true", default=False, help="Perform oracle evaluation of nbest output (by default evaluates only the first parse)")
     ap.add_argument("--conll", action="store_true", default=False, help="Use CoNLL format (uses zpar tab-output by default)")
+    ap.add_argument("--label-errors", action="store_true", default=False, help="Print an error summary by label")
 
     args = ap.parse_args()
     if args.conll:
